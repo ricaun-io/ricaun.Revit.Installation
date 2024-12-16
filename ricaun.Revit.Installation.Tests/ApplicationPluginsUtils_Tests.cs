05d50358c51febf053d77fda1049dd43d97af58f
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-12-16 16:09:43 -0300

    Update `ApplicationPluginsUtils` to download with `Mutex`

--- ricaun.Revit.Installation.Tests/ApplicationPluginsUtils_Tests.cs
+++ ricaun.Revit.Installation.Tests/ApplicationPluginsUtils_Tests.cs
@@ -1,13 +1,45 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ricaun.Revit.Installation.Tests
 {
     public class ApplicationPluginsUtils_Tests
     {
         [Test]
+        public async Task ApplicationPluginsUtils_Test_Download_Async()
+        {
+            var projectName = "RevitAddin.DA.Tester";
+            var bundleUrl = $@"https://github.com/ricaun-io/{projectName}/releases/latest/download/{projectName}.bundle.zip";
+
+            var applicationPluginsFolder = RevitUtils.GetCurrentUserApplicationPluginsFolder();
+            var bundleName = Path.GetFileNameWithoutExtension(bundleUrl);
+
+            Console.WriteLine($"DownloadBundle: {bundleName}");
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 5; i++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    await Task.Delay(0);
+                    ApplicationPluginsUtils.DownloadBundle(applicationPluginsFolder, bundleUrl, (e) =>
+                    {
+                        Console.WriteLine(e);
+                        Assert.Fail(e.Message);
+                    }, (log) => {
+                        //Console.WriteLine(log);
+                    });
+                });
+                tasks.Add(task);
+            }
+            await Task.WhenAll(tasks);
+        }
+
+            [Test]
         public void ApplicationPluginsUtils_Test_Download()
         {
             var projectName = "RevitAddin.DA.Tester";
