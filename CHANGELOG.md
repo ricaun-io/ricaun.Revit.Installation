af64e5dd5e77f39ab9daefac61c7d99749e8073b
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-03-05 11:49:16 -0300

    Add `FinalPathNameExtension`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.3.2] / 2025-03-05
+### Features
+### Updates
+- Add `FinalPathNameExtension` to fix localized `InstallLocation`. (Fix: #13)
+### Tests
+- Add `FinalPathName_Tests` with `english`, `portuguese` and `german` tests.
+
 ## [1.3.1] / 2024-12-16
 ### Features
 - `ApplicationPluginsUtils` with `Mutex` to prevent multiple instances. (Fix: #11)
@@ -82,6 +89,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - `ricaun.Revit.Installation.Tests`
 
 [vNext]: ../../compare/1.0.0...HEAD
+[1.3.2]: ../../compare/1.3.1...1.3.2
 [1.3.1]: ../../compare/1.3.0...1.3.1
 [1.3.0]: ../../compare/1.2.0...1.3.0
 [1.2.0]: ../../compare/1.1.2...1.2.0
---
+++ ricaun.Revit.Installation/Utils/FinalPathNameExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ricaun.Revit.Installation.Utils
+{
+    internal static class FinalPathNameExtension
+    {
+        private static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+
+        private const string DLL_NAME = "Kernel32.dll";
+        private const uint FILE_READ_EA = 0x0008;
+        private const uint FILE_FLAG_BACKUP_SEMANTICS = 0x2000000;
+
+        [DllImport(DLL_NAME, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern uint GetFinalPathNameByHandle(
+            IntPtr hFile,
+            [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszFilePath,
+            uint cchFilePath,
+            uint dwFlags);
+
+        [DllImport(DLL_NAME, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport(DLL_NAME, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CreateFile(
+                [MarshalAs(UnmanagedType.LPTStr)] string filename,
+                [MarshalAs(UnmanagedType.U4)] uint access,
+                [MarshalAs(UnmanagedType.U4)] FileShare share,
+                IntPtr securityAttributes, // optional SECURITY_ATTRIBUTES struct or IntPtr.Zero
+                [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+                [MarshalAs(UnmanagedType.U4)] uint flagsAndAttributes,
+                IntPtr templateFile);
+
+        public static string GetFinalPathName(string path, bool removeLongPathPrefix = true)
+        {
+            const int bufferSize = 1024;
+
+            var fileHandle = CreateFile(path,
+                FILE_READ_EA,
+                FileShare.ReadWrite | FileShare.Delete,
+                IntPtr.Zero,
+                FileMode.Open,
+                FILE_FLAG_BACKUP_SEMANTICS,
+                IntPtr.Zero);
+
+            if (fileHandle == INVALID_HANDLE_VALUE)
+                throw new FileNotFoundException("File not found.", path);
+
+            try
+            {
+                var fileStringBuilder = new StringBuilder(bufferSize);
+                var res = GetFinalPathNameByHandle(fileHandle, fileStringBuilder, bufferSize, 0);
+
+                if (res == 0)
+                    throw new FileNotFoundException("FinalPathNameByHandle fail.", path);
+
+                var finalPath = fileStringBuilder.ToString();
+
+                if (removeLongPathPrefix)
+                {
+                    // Remove the \\?\ prefix for the long path
+                    finalPath = finalPath.StartsWith(@"\\?\") ? finalPath.Substring(4) : finalPath;
+                }
+
+                return finalPath;
+            }
+            finally
+            {
+                CloseHandle(fileHandle);
+            }
+        }
+    }
+}
